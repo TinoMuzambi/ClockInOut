39957026a4143154cde24a82c6b9096f967c6764
TinoMuzambi <tinomuzambi@gmail.com>
2024-12-29 14:43:26 +0200

    Renames notes fields.

--- backend/api.R
+++ backend/api.R
@@ -25,8 +25,30 @@ data <- data %>%
          clock.in = parse_date_time(clock.in, orders = "%H:%M"),
          clock.out = parse_date_time(clock.out, orders = "%H:%M"))
 
+# Rename notes fields
+data.tidy <- data %>% 
+  mutate(notes = gsub(" ", "_", notes)) %>% 
+  mutate(notes = gsub(":", "_", notes)) %>% 
+  mutate(notes = gsub("-", "_", notes))
+
 # Separate options from notes into separate columns
-separated.notes <- data %>% separate_rows(notes, sep = ", ")
+data.tidy <- data.tidy %>% separate_rows(notes, sep = ",_")
+
+# List out note options
+notes.options <- unique(data.tidy$notes)[-1]
+
+# Field-friendly names
+notes.options.field.names <- gsub(" ", "_", notes.options)
+
+data.tidy <- data.tidy %>% 
+  mutate(across(all_of(notes.options), ~
+                  if_else(notes == ., TRUE, FALSE),
+                .names = "{.col}"))
+data.tidy
+
+data.tidy <- data.tidy %>% 
+  mutate(wfh = if_else(notes == "Work from home", TRUE, FALSE))
+data.tidy
 
 # View structure of data
 str(data)
