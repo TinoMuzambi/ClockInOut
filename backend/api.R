6ec43cba0d92bc0b5ba9a51719cd5122ec0b3d8d
TinoMuzambi <tinomuzambi@gmail.com>
2024-12-29 15:00:27 +0200

    Creates indicator columns for note options

--- backend/api.R
+++ backend/api.R
@@ -35,20 +35,30 @@ data.tidy <- data %>%
 data.tidy <- data.tidy %>% separate_rows(notes, sep = ",_")
 
 # List out note options
-notes.options <- unique(data.tidy$notes)[-1]
+# notes.options <- unique(data.tidy$notes)[-1]
+# notes.options <- c("Work_from_home", "Post_Work_Commitment",
+#                    "Pre_Work_Commitment", "16_00_lecture", "14_00_lecture",
+#                    "Public_Holiday", "Annual_leave", "11_00_lecture",
+#                    "Study_leave", "Conference", "Sick_leave")
 
-# Field-friendly names
-notes.options.field.names <- gsub(" ", "_", notes.options)
+# data.tidy <- data.tidy %>% 
+#   mutate(across(all_of(notes.options), ~
+#                   if_else(notes == ., TRUE, FALSE),
+#                 .names = "{.col}"))
 
+# Create indicator columns for note options
 data.tidy <- data.tidy %>% 
-  mutate(across(all_of(notes.options), ~
-                  if_else(notes == ., TRUE, FALSE),
-                .names = "{.col}"))
-data.tidy
-
-data.tidy <- data.tidy %>% 
-  mutate(wfh = if_else(notes == "Work from home", TRUE, FALSE))
-data.tidy
+  mutate(note_wfh = if_else(notes == "Work_from_home", TRUE, FALSE),
+         note_post.wc = if_else(notes == "Post_Work_Commitment", TRUE, FALSE),
+         note_pre.wc = if_else(notes == "Pre_Work_Commitment", TRUE, FALSE),
+         note_lecture.1600 = if_else(notes == "16_00_lecture", TRUE, FALSE),
+         note_lecture.1400 = if_else(notes == "14_00_lecture", TRUE, FALSE),
+         note_lecture.1100 = if_else(notes == "11_00_lecture", TRUE, FALSE),
+         note_annual.leave = if_else(notes == "Annual_leave", TRUE, FALSE),
+         note_sick.leave = if_else(notes == "Sick_leave", TRUE, FALSE),
+         note_study.leave = if_else(notes == "Study_leave", TRUE, FALSE),
+         note_holiday = if_else(notes == "Public_Holiday", TRUE, FALSE),
+         note_conference = if_else(notes == "Conference", TRUE, FALSE),)
 
 # View structure of data
 str(data)
